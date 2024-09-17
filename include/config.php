b792a23cb1b853f2c50abe9a66851d05219ac946
Martin Wadeson <martin.wadeson@ccsw.co.uk>
2024-09-17 21:49:10 +0100

    Began story parsing logic, added twig templates and styling base

--- include/config.php
+++ include/config.php
@@ -5,6 +5,11 @@ define('DIR_INCLUDE', DIR_ROOT . 'include/');
 define('DIR_CLASSES', DIR_ROOT . 'classes/');
 define('DIR_CLASSES_ABSTRACT', DIR_CLASSES . 'abstract/');
 define('DIR_VENDOR', DIR_ROOT . 'vendor/');
+define('DIR_TPL', DIR_ROOT . 'tpl/');
+define('DIR_CACHE', '/tmp/');
+define('DIR_CSS', DIR_ROOT . 'css/');
+
+define('SITE_CSS', '/css/');
 
 define('DB_USER', 'cddastorybrowser');
 define('DB_PASS', 'ywqe)EBJa#-Gp~@N6<[R?d');
@@ -12,4 +17,13 @@ define('DB_NAME', 'cddastorybrowser');
 define('DB_HOST', 'localhost');
 define('DB_TYPE', 'mysql');
 
+$dev = 0;
+if(isset($_SERVER['HTTP_HOST'])){
+	switch ($_SERVER['HTTP_HOST']) {
+		case 'cddastory.local':
+			$dev = 1;
+			break;
+	}
+}
+define('ISDEV', $dev);
 ?>
--- index.php
+++ index.php
@@ -1,11 +1,51 @@
 <?php
+// header('Content-Type: text/plain');
 include(dirname(__FILE__) . '/include/config.php');
 include(dirname(__FILE__) . '/include/autoload.php');
 
-$story = new story();
-$storyid = false;
-if(isset($_GET) && isset($_GET['id']) && is_numeric($_GET['id'])){
-	$storyid = intval($_GET['id']);
+$urlpath = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+$urlpath = explode('/', $urlpath);
+$page = current($urlpath);
+
+$output = $pagevars = array();
+$pagevars['stylesheets'] = array('main');
+
+switch ($page) {
+	case '':
+	default:
+		$story = new story();
+		$storyid = false;
+		if(!empty($urlpath) && isset($urlpath[0]) && is_numeric($urlpath[0])){
+			$storyid = $urlpath[0];
+		}
+		$story->loadStory($storyid);
+		$pagevars['story'] = &$story;
+		$template = 'story.twig';
+		$pagevars['stylesheets'][] = 'story';
+		break;
+	
+	case 'listing':
+		$template = 'listing.twig';
+		$pagevars['stylesheets'][] = 'story';
+		break;
 }
-$story->loadStory($storyid);
+
+
+
+$loader = new \Twig\Loader\FilesystemLoader(array(DIR_TPL, DIR_TPL . 'include/'));
+$twig = new \Twig\Environment($loader, array(
+	'cache' => DIR_CACHE,
+	'debug' => (bool) ISDEV
+));
+if(ISDEV){
+	$twig->addExtension(new \Twig\Extension\DebugExtension());
+}
+
+$output[] = $twig->render('header.twig', $pagevars);
+$output[] = $twig->render($template, $pagevars);
+$output[] = $twig->render('footer.twig', $pagevars);
+
+$output = implode('', $output);
+print $output;
+
 ?>
