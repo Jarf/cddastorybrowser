5e272717e521c80db50df0d59e8fa2097954bf49
Martin Wadeson <martin.wadeson@gmail.com>
2024-09-17 21:49:10 +0100

    Began story parsing logic, added twig templates and styling base

--- classes/story.php
+++ classes/story.php
@@ -13,7 +13,7 @@ class story extends Entity{
 	public function loadStory(int $id = null){
 		$sql = 'SELECT ' . implode(', ', $this->prependColumns()) . ', categories.name AS categoryName FROM ' . $this->table . ' JOIN categories ON stories.category = categories.id WHERE categories.descriptor = 0';
 		if(!empty($id)){
-			$sql .= ' AND id = :id';
+			$sql .= ' AND ' . $this->table . '.id = :id';
 		}else{
 			$sql .= ' ORDER BY RAND()';
 		}
@@ -27,6 +27,19 @@ class story extends Entity{
 			$row = $this->db->fetch();
 			foreach($row as $key => $val){
 				$this->$key = $val;
+			}
+		}
+		$this->parseStory();
+	}
+
+	public function parseStory(){
+		$this->story = nl2br($this->story);
+		// Replace color tags with styled spans
+		preg_match_all('/\<color_(\w+)\>(.*)\<\/color\>/ms', $this->story, $colormatches);
+		if(!empty($colormatches)){
+			foreach($colormatches[0] as $cmkey => $cmval){
+				$newstring = '<span style="color: ' . $colormatches[1][$cmkey] . '">' . $colormatches[2][$cmkey] . '</span>';
+				$this->story = str_replace($colormatches[0][$cmkey], $newstring, $this->story);
 			}
 		}
 	}
