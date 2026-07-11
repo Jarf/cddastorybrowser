1a95e1ab785c3cb6e1bcf48cce10f821764d6d6e
Martin Wadeson <martin.wadeson@gmail.com>
2026-07-11 11:59:00 +0100

    Prevent overflow on mobile displays

--- js/index.js
+++ js/index.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
 	if($('table#categoryIndex').length){
 		$('table#categoryIndex').DataTable({
-			order: [[1, 'desc']]
+			order: [[1, 'desc']],
+			responsive: true
 		});
 	}else if($('table#storyIndex').length){
 		const categoryIndex = $('input#categoryId').val();
 		const categoryName = $('input#categoryName').val();
 		const dt = $('table#storyIndex').DataTable({
-			order: []
+			order: [],
+			responsive: true
 		});
 	}
 });
